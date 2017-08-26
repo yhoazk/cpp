128fb3811b64443dafce9fa373e63f905556c408
yhoazk <yhoazk@gmail.com>
2017-08-26 13:51:45 -0500

    Map handling and extension

--- stl_tut/hello_map/hello_map.cpp
+++ stl_tut/hello_map/hello_map.cpp
@@ -2,6 +2,7 @@
 #include <map>
 #include <string>
 #include <vector>
+#include <exception>
 using namespace std;
 
 
@@ -20,6 +21,35 @@ void map_of_vectors(void)
 }
 
 
+void init_map(void)
+{
+  map<string, int > v = {{"one",1},{"two",2},{"three",3},{"four",4}};
+  /* Insert unnamed vectors in the map */
+
+  std::cout << "v['one']: " << v["one"] <<  std::endl;
+  std::cout << "v['two']: " << v["two"] <<  std::endl;
+  std::cout << "v['three']: " << v["three"] <<  std::endl;
+  std::cout << "v['four']: " << v["four"] <<  std::endl;
+  std::cout << "Delete elements by key:" << "\n\n";
+
+  std:: cout << "When a map is indicated to return a value of an unknown key it adds the key to the map and attaches null as \n its value, without trowing an exception\n";
+  std::cout << "v.erase('one')" << std::endl;
+  v.erase("one");
+  try{
+    std::cout << v["ddddone"] << " - Map size: " << v.size() << std::endl;
+    std::cout << v["ddddoneffff"] << " - Map size: " << v.size() << std::endl;
+    std::cout << v["ddddonefff"] << " - Map size: " << v.size() << std::endl;
+    std::cout << v["ddddonffff"] << " - Map size: " << v.size() << std::endl;
+    std::cout << v["dddddsdsoneffff"] << " - Map size: " << v.size() << std::endl;
+
+    std::cout <<  "So if we are trying to probe that a map has certain key we must use the map::count() function.\n this function returns the count of elements with an equivalent key\n ";
+  }
+  catch(exception&   e)
+  {
+    std::cout <<  "Catched exception: " <<  e.what() << std::endl;
+  }
+}
+
 int main(int argc, char const *argv[]) {
   map<char, string> map_test;
 
@@ -34,7 +64,7 @@ int main(int argc, char const *argv[]) {
   std::cout << "map_test[d]: " << map_test['d']  <<  std::endl;
   std::cout << "map_test contains" << map_test.size() << " elements." <<  std::endl;
 
-map_of_vectors();
-
+  map_of_vectors();
+  init_map();
   return 0;
 }
