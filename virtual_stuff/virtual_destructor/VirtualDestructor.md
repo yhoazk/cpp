397c948730230541dcdb6b5bbe1eab3064246e68
yhoazk <yhoazk@gmail.com>
2018-07-29 22:36:38 +0200

    Virtual destructors for inherited objects

--- virtual_stuff/virtual_destructor/VirtualDestructor.md
+++ virtual_stuff/virtual_destructor/VirtualDestructor.md
@@ -2,7 +2,8 @@
 
 
 A class with at least one vitual pure functions is abstract and it can't 
-be used to create a new object from it. 
+be used to create a new object from it. This means that the class was meant to
+be inherited. 
 
 ```cpp
 virtual void f() = 0; // This is a pure virtual function
@@ -13,4 +14,11 @@ The `=0` mark a function as a pure virtual
 
 When a parent class has virutal functions, the derived classes do **not**
 need to implement **all** the virtual methods, but they *do* need to 
-implement the pure ones. 
+implement the pure ones.
+
+The destructor of a class has to be virtual if we intend to inherit from it.
+Otherwise the destructir of the derived class will not be called when you 
+delete an object through a base pointer, and you'll get corrupted program
+states like memory leaks.
+
+
--- virtual_stuff/virtual_destructor/virtual_destructor.cpp
+++ virtual_stuff/virtual_destructor/virtual_destructor.cpp
@@ -1,7 +1,45 @@
 #include <iostream>
 
+class nonVirtDestrBase{
+  public:
+  ~nonVirtDestrBase(){
+    std::cout << "In base destructor" << std::endl;
+  }
+
+  int getVal() { return 222; }
+
+};
+
+class derived : public nonVirtDestrBase{
+  public:
+  ~derived(){
+    std::cout << "in derived destructor" << std::endl;
+  }
+  
+  int getVal(){
+    return (int) 9;
+  }
+
+};
+
+
 int main(int, char**)
 {
-    std::cout << "works";
-    5==5;
+    // object with automatic storage, mainly stack
+    {
+      derived d;
+      d.getVal();
+    } // As the object is in automatic storage and is not longer referenced is deleted.
+    std::cout << "flushing" << std::endl;
+    // object with dynamic stirage, mainly the heap.
+    derived* dd = new derived();
+    dd->getVal();
+    delete dd;
+    std::cout << "Now the test for the derived class destructor\n";
+    nonVirtDestrBase* base = new derived();
+    base->getVal();
+    delete base;
+    std::cout << "Here the derived constructor was never called, only the base constructor\n";
+    // in which case is expected to have a derived class assigned to a base class
+    return 0;
 }
