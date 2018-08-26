90a54c6593f0caf8a50584afe973b9761f6e5f32
yhoazk <yhoazk@gmail.com>
2018-08-26 14:47:56 +0200

    Example to capture the log of a binary
    Using bazel, pytest and subprocess capture the output from the subprocess
    and parse it.

--- bazel/pytest_bin/test/test_logger.py
+++ bazel/pytest_bin/test/test_logger.py
@@ -2,16 +2,32 @@ import pytest
 import subprocess
 import bubblewrap
 import sys, os
+import re
 
-
-def test_logger():
+def test_logger(capsys):
     print(os.listdir("."))
-    logger = subprocess.Popen("logger_generator")
-    logger.communicate()
-    assert True
+    #with open("log_gen") as log:
+    logger = subprocess.Popen("logger_generator", stdout=subprocess.PIPE)
+    out,err=logger.communicate()
+    # Communicate returns a bunch of bytes, we need to parse them to ascii
+    out = [l.decode("ascii") for l in out.splitlines()]
+    logged_text = []
+    print("-------- Out --------")
+    for l in out:
+        res = re.findall("thid:\d+\s+N:\s+\d+", l)
+        if len(res) == 1:
+            print(res)
+        else:
+            print("Malformed log: {}".format(l))
+    print("-------- Out --------")
 
-def test_logg_fail():
+    print("-------- Err --------")
+    print(err)
+    print("-------- Err --------")
     assert False
 
-test_logger()
-test_logg_fail()
+def test_logg_fail():
+    assert True
+
+if __name__ == "__main__":
+    sys.exit(pytest.main([__file__]))
