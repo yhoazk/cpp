10bbeaf50e339be30716fc62d580149dbd2b310e
yhoazk <yhoazk@gmail.com>
2018-11-08 23:11:27 +0100

    sort inste

--- smart_ptrs/unique_ptr/unique_ptr.cpp
+++ smart_ptrs/unique_ptr/unique_ptr.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
-//#include <memory>
+#include <memory>
+/* Here we will have an error at compile time as the copy constructor was 
+ * deleted from the base obj
+ * */
 
 int main(){
     std::unique_ptr<int> a(new int);
