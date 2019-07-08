f1bfe8ade32d849ab28a576c2aac75152812ee23
yhoazk <yhoazk@gmail.com>
2019-07-08 23:00:41 +0200

    Finnaly filtering map

--- stl_tut/maps/filter_by_prop/filter_by_prop.cpp
+++ stl_tut/maps/filter_by_prop/filter_by_prop.cpp
@@ -13,10 +13,12 @@ struct iface_properties {
 };
 
 using ip_proprerties = map<string, iface_properties>;
-vector<pair<string, iface_properties&> > filtered_ifaces;
+//vector<pair<const string, iface_properties>&> filtered_ifaces;
+vector<pair<const string, iface_properties>> filtered_ifaces;
 ip_proprerties ip_settings;
 
-void print_dict(const ip_proprerties& ip_cfg){
+template<class T>
+void print_ifconfig(const T& ip_cfg){
   for(auto& entry : ip_cfg){
     cout << "Name: " << entry.first;
     cout << " IP: " << dec;
@@ -31,7 +33,6 @@ void print_dict(const ip_proprerties& ip_cfg){
     cout << "\t running " << entry.second.running << '\n';
   }
 }
-
 
 int main(int argc, char const *argv[])
 {
@@ -49,13 +50,15 @@ int main(int argc, char const *argv[])
   // remove if cannot be used as the map elements contain a const as
   // a key, which does not fit with the remove_if
   //remove_if(begin(ip_settings), end(ip_settings), noloop_and_up);
-  for (auto& element : ip_settings){
+  for (auto element : ip_settings){
     if(!element.second.loopback && element.second.running){
       filtered_ifaces.push_back(element);
     }
   }
 
   cout << ip_settings.size();
-  print_dict(ip_settings);
+  print_ifconfig(ip_settings);
+  cout << "------\n";
+  print_ifconfig(filtered_ifaces);
   return 0;
 }
