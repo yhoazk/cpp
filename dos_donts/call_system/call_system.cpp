18a569fcf9dcef4b4fad316f1bf58a93716a5c9a
yhoazk <yhoazk@gmail.com>
2019-10-20 15:37:05 +0200

    The script still waits for completion

--- dos_donts/call_system/call_system.cpp
+++ dos_donts/call_system/call_system.cpp
@@ -4,14 +4,18 @@
 #include <cstring>
 #include <vector>
 #include <cerrno>
+#include <fcntl.h>
 
 namespace my
 {
+    // Why use popen when the string is not needed?
     bool pop(std::string cmd){
         FILE *process {0};
         char response[1024];
         bool call_status{false};
         process = popen(cmd.c_str(), "r");
+        int fd_process = fileno(process);
+        fcntl(fd_process, F_SETFL, O_NONBLOCK);
         if(process == 0){
             std::cerr << "popen: " << strerror(errno) ;
             return false;
@@ -23,6 +27,9 @@ namespace my
         // on error returns -1
         // the return value of the children process is given on the 
         // bits [7:15] second byte
+        // we need to poll for this process to end and not block this 
+        // thread. The pclose should be non-blocking to allow the program
+        // to continue
         int shell_status = pclose(process);
         if(-1 == shell_status){
             std::cerr << "pclose call failed: " << strerror(errno);
@@ -30,15 +37,17 @@ namespace my
             std::cerr << "Script finished with errno: " << strerror(errno) << '\n';
             std::cout << "shell stat: " << static_cast<int>((shell_status & 0xff00) >> 8) << '\n';
         } 
-
         return call_status;
     }
 } // namespace 
 
 int main(){
     /* simple systemcall */
-//    for(int k =5; k>0;--k)
 //        system("./my_script.sh");
-    my::pop("./my_script.sh");
+    // this way it will block on every call and wait for finish
+    // what is needed is to verify if the script is already running
+    // and do exit of true
+   for(int k =5; k>0;--k)
+        my::pop("./my_script.sh &");
     return 0;
 }
--- dos_donts/call_system/my_script.sh
+++ dos_donts/call_system/my_script.sh
@@ -3,11 +3,12 @@
 # this script will be called several times, but it will fail in the flock part
 ## getting the error status of a subshell
 ## https://blog.famzah.net/2013/07/31/using-flock-in-bash-without-invoking-a-subshell/
+echo "before"
 (
     flock  -n 999 || exit
     ## this script will call other binaries with different and network configs
     ## how to keep track of this?
     ./another_script.sh &
-    wait $! && exit $?
+    wait $! && exit $? # here we send the exit status of the other script to uppershell
 ) 999>/tmp/dhcpc_lock
 exit $? # This is the value seen by the c++ program, which comes from the subshell
