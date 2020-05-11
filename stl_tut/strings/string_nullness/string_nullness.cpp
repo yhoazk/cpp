2dc22650e2ca22bc30cb67a5e0e58e0413b866ac
yhoazk <yhoazk@gmail.com>
2020-05-12 00:21:51 +0200

    Static assert also not working

--- stl_tut/strings/string_nullness/string_nullness.cpp
+++ stl_tut/strings/string_nullness/string_nullness.cpp
@@ -1,15 +1,28 @@
 #include <iostream>
 #include <string>
+#include <type_traits>
 // The problem heres is that the string is not decided at compile time
 // a string is created and the a reference is passed to this fnc at run
 // time, because of this is not possible to chech for nullness in this call
 bool test_null( const std::string& cstr ) {
-    std::cout << cstr << '\n';
-    return cstr.empty();
+    static_assert(std::is_null_pointer<decltype(cstr)>::value == false);
+    //if(cstr != nullptr) {
+    if( not cstr.empty()) {
+        std::cout << cstr << '\n';
+        return cstr.empty();
+    } else { return false;  }
 }
 
 
-int main() {
+int main(int c, char** cv) {
+    char* var;
+    std::cout << "c: " << std::to_string(c) << '\n';
+    if(c > 1) {
+        var = nullptr;
+    } else {
+     //   var = "Taco\n";
+    }
+    test_null("dsdasd");
     test_null(nullptr);
     return 0;
 }
