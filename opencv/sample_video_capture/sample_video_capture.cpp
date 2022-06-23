b685f0a1ae92e7870367a68361f0c53701345158
yhoazk <yhoazk@gmail.com>
2022-06-23 23:45:09 +0200

    opencv samples

---
+++ opencv/sample_video_capture/sample_video_capture.cpp
@@ -0,0 +1,21 @@
+// #include <opencv2/opencv.hpp>
+#include <time.h>
+#include <vector>
+#include <stdlib.h>
+#include <string>
+
+// #include <opencv2/core/core.hpp>
+// #include <opencv2/core/types_c.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/video.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main() {
+    VideoCapture cap("sample.avi");
+    return 0;
+}
